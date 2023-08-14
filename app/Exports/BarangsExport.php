5519d5b35c3751a211707ca373c1f301e33caf17
unknown <neet693@gmail.com>
2023-08-15 05:28:56 +0700

    Fix Export Heading

--- app/Exports/BarangsExport.php
+++ app/Exports/BarangsExport.php
@@ -4,12 +4,33 @@ namespace App\Exports;
 
 use App\Models\Barang;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class BarangsExport implements FromCollection
+class BarangsExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
+
+    public function headings(): array
+    {
+        return [
+            '#',
+            'Kode Barang',
+            'Nama Barang',
+            'Merk',
+            'Spesifikasi',
+            'Tanggal',
+            'Kondisi',
+            'Jumlah',
+            'Sumber Dana',
+            'Created At',
+            'Updated At',
+            'Kategorial',
+            'Ruangan ID',
+            'Jenis Pengadaan ID',
+        ];
+    }
     public function collection()
     {
         return Barang::all();
