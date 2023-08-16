328f5fd2aac96bdc9fab1fd4d82babe6cd177331
unknown <neet693@gmail.com>
2023-08-16 10:05:36 +0700

    Layout fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,6 @@
                                             class="bi bi-house"></i>
                                         Home</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Link</a>
-                                </li>
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" role="button"
                                         data-bs-toggle="dropdown" aria-expanded="false">
